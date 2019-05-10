4353dec4129cae39aa0436d65c395d916d12e91b
sayaka-tamura <arccycle@gmail.com>
2019-05-10 17:33:59 +0900

    create counter classes

---
+++ src/com/in28minutes/concurrency/BiCounter.java
@@ -0,0 +1,25 @@
+package com.in28minutes.concurrency;
+
+public class BiCounter {
+
+	private int i = 0;
+
+	private int j = 0;
+
+	synchronized public void incrementI() {
+		i++;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	synchronized public void incrementJ() {
+		j++;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+}
